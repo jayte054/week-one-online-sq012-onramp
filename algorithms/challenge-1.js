eb17607d59684815990dd8b4e15db9a596e2478a
jayte054 <54476858+jayte054@users.noreply.github.com>
2022-06-23 03:47:56 +0100

    Update challenge-1.js

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,4 +1,18 @@
 function secondLargest(arr) {
+    let largest = 0;
+    let secondLargest = 0;
+    let[] arr = (7, 2, 5, 54, 17);
+
+    for (let i =0; i < arr.length, i++) {
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest= arr[1];
+        }else if (arr[i] > secondLargest) {
+            secondLargest = arr[i];
+        }
+    }
+    return secondLargest(arr);
   
 }
+
 module.exports = secondLargest
